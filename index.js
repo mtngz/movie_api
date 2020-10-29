c8bd49badf23327094d0208220a1c58850c5e909
mtngz <70101199+mtngz@users.noreply.github.com>
2020-10-29 19:42:51 +0100

    Create request get user by username in index.js

--- index.js
+++ index.js
@@ -107,6 +107,18 @@ app.get("/users", (req, res) => {
     });
 });
 
+// Get a user by username
+app.get("/users/:Username", (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+
 // Put updates to user information
 app.put("/users/:userid/:username", (req, res) => {
   res.send(
