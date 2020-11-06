5b8322588a9ac18877d51588a582e7ac55180d4f
mtngz <70101199+mtngz@users.noreply.github.com>
2020-11-06 15:15:58 +0100

    add allowed Origins to CORS in index.js

--- index.js
+++ index.js
@@ -42,7 +42,24 @@ require("./passport");
 
 // CORS
 const cors = require("cors");
-app.use(cors());
+
+let allowedOrigins = ["http://localhost", "http://testsite.com"];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application does not allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 // GET requests
 
