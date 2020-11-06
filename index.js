e01ed9c5c6b2df18eb9364cad25eb1c5a77f191b
mtngz <70101199+mtngz@users.noreply.github.com>
2020-11-06 16:01:07 +0100

    add hashedPassword in index.js

--- index.js
+++ index.js
@@ -148,6 +148,7 @@ app.get(
 
 // Post new user registration
 app.post("/users", (req, res) => {
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
@@ -155,7 +156,7 @@ app.post("/users", (req, res) => {
       } else {
         Users.create({
           Username: req.body.Username,
-          Password: req.body.Password,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday,
         })
