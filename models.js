5d60c0984841e2d903931a42796a4ce8d6a79b8d
mtngz <70101199+mtngz@users.noreply.github.com>
2020-11-06 15:56:21 +0100

    add bcrypt to project

--- models.js
+++ models.js
@@ -1,33 +1,42 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose"),
+  bcrypt = require("bcrypt");
 
 // create a Movie Schema
 
 let movieSchema = mongoose.Schema({
-    Title: {type: String, required: true},
-    Description: {type: String, required: true},
-    Phase: {
-        Name: String,
-        Description: String
-    },
-    Director: {
-        Name: String,
-        Bio: String,
-        Birth: Number,
-        Death: Number
-    },
-    ImagePath: String,
-    Featured: Boolean
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Phase: {
+    Name: String,
+    Description: String,
+  },
+  Director: {
+    Name: String,
+    Bio: String,
+    Birth: Number,
+    Death: Number,
+  },
+  ImagePath: String,
+  Featured: Boolean,
 });
 
 // Create a User Schema
 
 let userSchema = mongoose.Schema({
-    Username: {type: String, required: true},
-    Password: {type: String, required: true},
-    Email: {type: String, required: true},
-    Birthday: Date,
-    Favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  Favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
 });
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
 // initiate schemata
 
