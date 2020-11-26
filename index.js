d91a04ea2a96440576cf55856b66406feaaa7f44
root <root@DESKTOP-VUKLK3J.localdomain>
2020-11-26 14:57:09 +0100

    delete authentication for movies endpoint

--- index.js
+++ index.js
@@ -80,20 +80,16 @@ app.get("/", (req, res) => {
 });
 
 // Get a list of data about all movies
-app.get(
-  "/movies",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Movies.find()
-      .then((movies) => {
-        res.status(201).json(movies);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error: " + err);
-      });
-  }
-);
+app.get("/movies", (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
 // Get data about a single movie, by title
 app.get(
